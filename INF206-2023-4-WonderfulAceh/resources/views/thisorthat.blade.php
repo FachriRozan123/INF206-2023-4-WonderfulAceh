fb9781806fd34efc444bdaa1ecf75d9526c57348
Nabila Aprillia <nabila32@mhs.unsyiah.ac.id>
2023-04-14 01:06:10 +0700

    feat: add thisorthat file

---
+++ INF206-2023-4-WonderfulAceh/resources/views/thisorthat.blade.php
@@ -0,0 +1,127 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ThisOrThat</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/348c676099.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light " style="background-color: #2b6363;">
+            <div class="container-fluid">
+                <img src="img/logowondeful.png" style="height: 3%; width: 3%;">
+                <a class="navbar-brand">Wonderful Aceh
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                    <ul class="navbar-nav mb-2 mb-lg-0">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdown" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">Daftar
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">Tempat Wisata</a></li>
+                                <li><a class="dropdown-item" href="#">Tour Guide</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active text-light" aria-current="page" href="#">Kategori</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active text-light" aria-current="page" href="#">This Or That</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex" style="margin-right: 20px;">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                    <li class="nav-item dropdown" style="margin-right:30px;">
+                        <div style="position: relative; bottom:10px;" class="nav-link dropdown-toggle" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="fas fa-user-circle fa-2x me-2 text-white" style="list-style: none;"></i>
+                            <span class="text-white" style="position: relative;bottom: px;"></span>
+                        </div>
+                        <ul class="dropdown-menu dropdown-menu-end">
+                            <li><a class="dropdown-item" href="#">Profile</a></li>
+                            <li><a class="dropdown-item" href="#">Setting</a></li>
+                            <li><a class="dropdown-item" href="#">Log Out</a></li>
+                        </ul>
+                    </li>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <main style=" background-color:#1f4847  !important;">
+        <br>
+        <br>
+        <br>
+        <div style="text-align: center;">
+            <h1 style="font-size:3rem;color:white;">
+                This Or That
+            </h1>
+            <br>
+            <br>
+            <div class="row" style="justify-content: center;">
+                <div class="col" style="position: relative;left: 200px;">
+                    <a href=".html" class="text-decoration-none">
+                        <span
+                            style="display: inline-block; height: 250px; width: 250px; background-color: white; border-radius: 100%; overflow: hidden;padding: 50px center;">
+                            <img src="img/wisatahits.jpg" style="height: 100%; width: auto;">
+                        </span>
+                        <br>
+                        <br>
+                        <br>
+                        <p class="text-white">
+                            Pilihan 1
+                        </p>
+                    </a>
+                </div>
+                <div class="col">
+                    <span style="padding: 0 150px 0 150px;border-top: 1px solid white;position: relative;top: 120px;">
+                    </span>
+                </div>
+
+                <div class="col" style="position: relative;right: 200px;">
+                    <span
+                        style="display: inline-block; height: 250px; width: 250px; background-color: white; border-radius: 100%; overflow: hidden;padding: 50px center;">
+                        <img src="img/pantai.jpg" style="height: 100%; width: 100%;">
+                    </span>
+                    <br>
+                    <br>
+                    <br>
+                    <p class="text-white">
+                        Pilihan 2
+                    </p>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+    </main>
+
+    <footer class="bg-white text-center pt-">
+        <p> Copyright 2023 &copy; WonderfulAceh</p>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
