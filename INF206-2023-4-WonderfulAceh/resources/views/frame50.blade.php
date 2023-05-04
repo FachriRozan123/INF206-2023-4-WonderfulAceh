ec3c4dcc90760b270ba3281b6ab8d4ea987d0a0c
teuku indris <indris@mhs.unsyiah.ac.id>
2023-05-04 21:07:07 +0700

    feat: change file frame 50, app and thisorthat

--- INF206-2023-4-WonderfulAceh/resources/views/frame50.blade.php
+++ INF206-2023-4-WonderfulAceh/resources/views/frame50.blade.php
@@ -1,43 +1,91 @@
 @extends('layouts.app')
 
 @section('content')
-<!doctype html>
-<html>
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <script src="https://cdn.tailwindcss.com"></script>
-</head>
-<body class="bg-[#306A6A]">
-    <p class="text-[#FFFFFF] ml-[100pt] text-[36px]">
-        Rekomendasi untuk anda
-    </p>
-    <div class="container mx-auto max-w-[1000pt] flex flex-col sm:flex-row h-[80pt] ">
-        <div class="container max-w-[200pt] bg-[#908B8B] flex justify-center">
-            <img class="scale-50" src="img/gallery.png">
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Tempat</title>
+    <!-- Bootstrap CDN Links -->
 
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
+    />
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <!-- Tailwind CSS CDN Links -->
+
+    <link
+      href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css"
+      rel="stylesheet"
+    />
+    <script
+      src="https://kit.fontawesome.com/348c676099.js"
+      crossorigin="anonymous"
+    ></script>
+   </head>
+  <body>
+    
+ <main class="w-screen h-screen p-5" style="background-color: #234741">
+    <p class="text-white ml-[100pt] text-xl font-bold font-serif p-2">
+        Rekomendasi untuk Anda
+</p>
+      <div class="card mb-3" style="max-width: 100%">
+        <div class="row">
+          <div class="col-auto">
+            <img
+              src="img/sungai.jpeg"
+              class="img-fluid rounded-start"
+              alt="Foto error"
+              style="background-position: center; width: 280px; height: 180px;"
+            />
+          </div>
+          <div class="col">
+            <div class="card-body">
+              <h5 class="card-title">Nama :</h5>
+              <h5 class="card-title">Kategori :</h5>
+              <h5 class="card-title">0.0/0.5 :</h5>
+            </div>
+          </div>
         </div>
-        <div class="container bg-[#C4C4C4] grid grid-cols-4 grid-rows-3 items-center">
-            <div class="col-start-1 ml-10">Nama :</div>
-            <div class="col-start-1 row-start-2 ml-10">Kategori :</div>
-            <div class="col-start-1 row-start-3 ml-10">0.0/5.0 :</div>
-
+      </div>
+      <div class="card mb-3" style="max-width: 100%">
+        <div class="row">
+          <div class="col-auto">
+            <img
+              src="img/pantai.jpg"
+              class="img-fluid rounded-start"
+              alt="Foto error"
+              style="background-position: center; width: 280px; height: 180px;"
+            />
+          </div>
+          <div class="col">
+            <div class="card-body">
+              <h5 class="card-title">Nama :</h5>
+              <h5 class="card-title">Kategori :</h5>
+              <h5 class="card-title">0.0/0.5 :</h5>
+            </div>
+          </div>
         </div>
-    </div>
+      </div>
+    </main>
+    <footer class="bg-white text-center bottom-full">
+      <p>Copyright 2023 &copy; WonderfulAceh</p>
+    </footer>
 
-    <div class="container mx-auto max-w-[1000pt] flex flex-col sm:flex-row h-[80pt] mt-[30pt]">
-        <div class="container max-w-[200pt] bg-[#908B8B] flex justify-center">
-            <img class="scale-50" src="img/gallery.png">
-
-        </div>
-        <div class="container bg-[#C4C4C4] grid grid-cols-4 grid-rows-3 items-center">
-            <div class="col-start-1 ml-10">Nama :</div>
-            <div class="col-start-1 row-start-2 ml-10">Kategori :</div>
-            <div class="col-start-1 row-start-3 ml-10">0.0/5.0 :</div>
-
-        </div>
-    </div>
-   
-</body>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+      crossorigin="anonymous"
+    ></script>
+  </body>
 </html>
+
 @endsection
