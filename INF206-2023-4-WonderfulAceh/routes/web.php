a5ced4f1ad10f7ebaab1770884b78d79d53e8a5c
teuku indris <indris@mhs.unsyiah.ac.id>
2023-05-15 22:45:06 +0700

    -

--- INF206-2023-4-WonderfulAceh/routes/web.php
+++ INF206-2023-4-WonderfulAceh/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TempatWisataController;
 use App\Http\Controllers\TourGuideController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\RekomendasiController;
 /*
 |--------------------------------------------------------------------------
@@ -15,46 +16,53 @@ use App\Http\Controllers\RekomendasiController;
 |
 */
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/', function () {
-        return redirect('/welcome');
-    });
-    
-    Route::get('/welcome', function () {
-        return view('welcome');
-    });
-    Route::get('/rekomendasi', [RekomendasiController::class, 'index'])->name('rekomendasi.index');
+Route::get('/', function () {
+    return view('welcome');
+});
 
-    // Rute untuk hasil pencarian
-    Route::get('/hasil-pencarian/{keyword}', [HasilPencarianController::class, 'index'])->name('hasil_pencarian');
-});
-Route::get('/category', function () {
-    return view('category');
-});
-Route::get('/search', function () {
-    return view('search');
-});
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified'
+// ])->group(function () {
+//     Route::get('/', function () {
+//         return redirect('/welcome');
+//     });
+//     Route::get('/welcome', function () {
+//         return view('welcome');
+//     });
+//     Route::get('/rekomendasi', [RekomendasiController::class, 'index'])->name('rekomendasi.index');
+
+//     // Rute untuk hasil pencarian
+//     Route::get('/hasil-pencarian/{keyword}', [HasilPencarianController::class, 'index'])->name('hasil_pencarian');
+// });
+Route::get('/category',[CategoryController::class,'index']);
+Route::get('/category/{slug}',[CategoryController::class, 'place']);
+// web.php
+
+Route::get('/search', [TempatWisataController::class, 'searchByName']);
+Route::get('/thisorthat', [TempatWisataController::class, 'thisorthat']);
+Route::post('/store-answer', [TempatWisataController::class, 'storeAnswer'])->name('storeAnswer');
 Route::get('/addTempat', function () {
     return view('addTempat');
 });
+Route::middleware(['auth'])->group(function () {
 Route::resource('holiday',TempatWisataController::class);
-
+Route::resource('tour_guide',TourGuideController::class);
+});
 Route::get('/addTourGuide', function () {
     return view('addTourGuide');
 });
-Route::get('/thisorthat', function () {
-    return view('thisorthat');
-});
+// Route::get('/thisorthat', function () {
+//     return view('thisorthat');
+// });
 Route::get('/nama_category', function () {
     return view('nama_category');
 });
 Route::get('/tempat', function () {
     return view('tempat');
 });
+Route::get('/tempat/{slug}',[TempatWisataController::class, 'place']);
 // Route::get('/login', function () {
 //     return view('login');
 // });
@@ -80,4 +88,3 @@ Route::get('/AboutUs', function () {
 Route::resource('tour_guide',TourGuideController::class);
 
 Route::get('/rekomendasi', [RekomendasiController::class, 'index'])->name('rekomendasi');
-Route::post('/search', [RekomendasiController::class, 'search'])->name('rekomendasi.search');
